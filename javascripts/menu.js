46094440b634c60879104aa3839f3b2158675b88
Aaron Janse <gitduino@gmail.com>
2016-12-10 12:28:52 -0800

    add firebug lite debugger
    accessible through game settings

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,3 +1,27 @@
+var addedDebug = false;
+
+function openDebug() {
+	// if(addedDebug) {
+	// 	return;
+	// }
+	//
+	// addedDebug = true;
+
+	(function() {
+		var scriptTag = document.createElement('script');
+	        scriptTag.type = "text/javascript";
+	        scriptTag.src = 'http://getfirebug.com/firebug-lite.js#startOpened';
+	        document.getElementsByTagName('head')[0].appendChild(scriptTag);
+	        var ready = function() {console.log('test');};
+	        scriptTag.onreadystatechange = function () {
+	            if (scriptTag.readyState == 'complete') {
+	                ready();
+	            }
+	        }
+	        scriptTag.onload = ready;
+	})()
+}
+
 function gohome() {
 	home=true
 //	ga('send', 'event', {
