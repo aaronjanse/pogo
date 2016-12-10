a0d525bcabfc0c97d0bb6f051865bd15c79ecf9e
Aaron Janse <gitduino@gmail.com>
2016-12-10 14:52:34 -0800

    add url debug parameter
    useful for catching errors caused before would have been opened in the
    menu

--- javascripts/main.js
+++ javascripts/main.js
@@ -228,6 +228,33 @@ function saveopts1(tutsave) {
 
 function onload() {
 
+  var QueryString = function () {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}();
+
+  if(QueryString.debug==="1") {
+    openDebug();
+  }
+
     window.mobileAndTabletcheck = function () {
         var check = false;
         (function (a) {
